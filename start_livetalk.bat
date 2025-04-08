784cfeb020daa02552b83793e00bc5aef8bfd58e
carterwayneskhizeine <got.money@qq.com>
2025-04-08 16:35:14 +0800

    更新依赖项，添加 python-dotenv 以支持环境变量管理；修改启动脚本中的 avatar_id 参数以使用新的头像配置。

--- start_livetalk.bat
+++ start_livetalk.bat
@@ -8,6 +8,8 @@ REM 切换到LiveTalking目录
 cd /d D:\Code\GoldieRillChat\LiveTalking
 
 REM 启动LiveTalking服务
-python app.py --transport webrtc --model wav2lip --avatar_id wav2lip256_avatar1 --tts cosyvoice
+python app.py --transport webrtc --model wav2lip --avatar_id wav2lip256_avatarNews --tts cosyvoice
 
 echo LiveTalking服务已关闭。 
+
+#http://127.0.0.1:8010/webrtc-combined.html
